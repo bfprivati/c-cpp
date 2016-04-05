0c749648055220c11b2ad189dec84a220c9f2af9
Bianca Privati <bfprivati@hotmail.com>
2016-04-05 02:55:24 -0300

    Funcoes a serem feitas

--- TRABALHO1 PT2/t1pt2.h
+++ TRABALHO1 PT2/t1pt2.h
@@ -9,3 +9,11 @@ int listaVazia(LISTA *L); 	//OK
 //verifica se a lista esta vazia
 LISTA* inserir(LISTA *L, int coef, int grau);  //OK
 //inserir ordenado na lista
+void imprimir(LISTA *L);
+//imprime o polinomio inversamente ao adicionado
+LISTA* excluiTermo(LISTA *L, int grau);
+//elimina o termo que contem a potencia igual à inserida pelo usuario
+LISTA* esvaziaLista(LISTA *L);
+//reinicializa o polinomio, zerando a lista
+int calcula(LISTA *L, int x);
+//calcula o polinomio inserido, usando o valor de x que o usuario inseriu
